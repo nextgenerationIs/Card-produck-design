ffcf72cee208472b568dd4a95bab8faa7493358f
muhammad ismail <muhismailmks@gmail.com>
2023-12-25 09:31:19 +0800

    add

---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log('hello wbr');
+
+type current = {
+    count : number
+}
+
+function sayHello({count} : current){
+    return count
+}
+
+
+let age: number = 10
+const boe: string = '10'
